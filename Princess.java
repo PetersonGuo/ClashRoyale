b273261c11178c5b67342cb3a5606fd1c1d10bb8
ke1b <121261293+ke1b@users.noreply.github.com>
2023-04-20 22:38:52 -0400

    comment

--- Princess.java
+++ Princess.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Normal here.
  * 
- * @author (your name) 
+ * @author Kelby To 
  * @version (a version number or a date)
  */
 public class Princess extends Towers
