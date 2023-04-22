076fdd4fe3d1810b4206562d7da0bc66cf1d96e9
KevinLuo0 <129564882+KevinLuo0@users.noreply.github.com>
2023-04-21 23:07:39 -0400

    Update EndScreen.java

--- EndScreen.java
+++ EndScreen.java
@@ -24,47 +24,18 @@ public class EndScreen extends Worlds {
             }
         }
         
-        if (allyTowers == EnemyTowers) {
-            if (towerHp > towerHp2 && allyTowers == 3) {
-                //blue side wins by having more hp
-                GreenfootImage end = new GreenfootImage("crBlue1Crown.png");
-            }else if (towerHp2 > towerHp && allyTowers == 3) {
-                //red side wins by having more hp
-                GreenfootImage end = new GreenfootImage("crRed1Crown.png");
-            }else if (towerHp > towerHp2 && allyTowers == 2) {
-                //blue side wins by having more hp
-                GreenfootImage end = new GreenfootImage("crBlue2Crown.png");
-            }else if (towerHp2 > towerHp && allyTowers == 2) {
-                //red side wins by having more hp
-                GreenfootImage end = new GreenfootImage("crRed2Crown.png");
-            }else if (towerHp > towerHp2 && allyTowers == 1) {
-                //blue side wins by having more hp
-                GreenfootImage end = new GreenfootImage("crBlue3Crown.png");
-            }else if (towerHp2 > towerHp && allyTowers == 1) {
-                //red side wins by having more hp
-                GreenfootImage end = new GreenfootImage("crRed3Crown.png");
+        if (allyTowers == EnemyTowers){
+            if (towerHp > towerHp2){
+                end = new GreenfootImage("blueWin.png");
+            }else if (towerHp2 > towerHp){
+                end = new GreenfootImage("redWin.png");
             }else{
-                //tie
-                GreenfootImage end = new GreenfootImage("crDraw.png");
+                end = new GreenfootImage("draw.png");
             }
-        }else if (allyTowers > EnemyTowers && EnemyTowers == 2) {
-            //blue side wins by 1 crown
-            GreenfootImage end = new GreenfootImage("crBlue1Crown.png");
-        }else if (allyTowers > EnemyTowers && EnemyTowers == 1) {
-            //blue side wins by 2 crown
-            GreenfootImage end = new GreenfootImage("crBlue2Crown.png");
-        }else if (allyTowers > EnemyTowers && EnemyTowers == 0) {
-            //blue side wins by 3 crown
-            GreenfootImage end = new GreenfootImage("crBlue3Crown.png");
-        }else if (EnemyTowers > allyTowers && allyTowers == 2) {
-            //red side wins by 1 crown
-            GreenfootImage end = new GreenfootImage("crRed1Crown.png");
-        }else if (EnemyTowers > allyTowers && allyTowers == 1) {
-            //red side wins by 2 crown
-            GreenfootImage end = new GreenfootImage("crRed2Crown.png");
-        }else if (EnemyTowers > allyTowers && allyTowers == 0) {
-            //red side wins by 3 crown
-            GreenfootImage end = new GreenfootImage("crRed3Crown.png");
+        }else if(allyTowers > EnemyTowers){
+            end = new GreenfootImage("blueWin.png");
+        }else{
+            end = new GreenfootImage("redWin.png");
         }
         
         end.scale(FINAL.WORLD_WIDTH, FINAL.WORLD_HEIGHT);
